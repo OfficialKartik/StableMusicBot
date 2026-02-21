09653f440d279d8d83e3a4fbf834c3c861eb5bd8
Kartik <45144906+OfficialKartik@users.noreply.github.com>
2026-02-21 15:27:48 +0530

    Update play.js

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -1,1 +1,46 @@
 
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  name: 'play',
+  description: 'Play a song from a URL or search term',
+
+  async execute(client, message, args) {
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) {
+      return message.reply('You must be in a voice channel.');
+    }
+
+    const query = args.join(' ');
+    if (!query) {
+      return message.reply('Provide a YouTube URL or search term.');
+    }
+
+    const state = client.engine.get(message.guild.id);
+
+    // Connect if not connected
+    if (!state.connection) {
+      await client.engine.connect(message.guild, voiceChannel);
+    }
+
+    // Add to queue
+    const track = {
+      title: query,
+      url: query,
+      requester: message.author.tag,
+    };
+
+    state.queue.push(track);
+
+    if (!state.current) {
+      await client.engine.playNext(message.guild.id);
+    }
+
+    const embed = new EmbedBuilder()
+      .setColor(client.config.embedColor)
+      .setTitle('Queued')
+      .setDescription(`**${query}** added to queue.`);
+
+    return message.channel.send({ embeds: [embed] });
+  },
+};
