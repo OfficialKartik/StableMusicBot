c4f6dbce495bc703ea72cb5f176a14e2f070a83d
Kartik <45144906+OfficialKartik@users.noreply.github.com>
2026-02-21 15:22:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 
+require('dotenv').config();
+
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const Engine = require('./core/engine');
+const config = require('./config');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+});
+
+client.config = config;
+client.commands = new Collection();
+client.engine = new Engine(client);
+
+// Load handlers
+require('./events/loader')(client);
+require('./commands/loader')(client);
+
+client.login(process.env.DISCORD_TOKEN);
